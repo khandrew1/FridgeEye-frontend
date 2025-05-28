3bad3e83e3cff05686a58c8fb97d855b346c12d4
Andrew Khadder <akhad007@ucr.edu>
2025-05-27 18:49:19 -0700

    add recognition

---
+++ recognition.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import jetson.inference
+import jetson.utils
+
+import argparse
+
+# parse the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect.")
+args = parser.parse_args()
+
+# load an image (into shared CPU/GPU memory)
+img = jetson.utils.loadImage(args.filename)
+
+# load the recognition network
+net = jetson.inference.imageNet(args.network)
+
+# classify the image
+class_idx, confidence = net.Classify(img)
+
+# find the object description
+class_desc = net.GetClassDesc(class_idx)
+
+predictions = net.Classify(img, topK=args.topK)
+
+for n, (classID, confidence) in enumerate(predictions):
+   classLabel = net.GetClassLabel(classID)
+   confidence *= 100.0
+   print(f"imagenet:  {confidence:05.2f}% class #{classID} ({classLabel})")
+
+# print out the result
+# print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))

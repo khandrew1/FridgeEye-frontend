4257d04566d177a6023870a4c2d4f438d0f27250
Andrew Khadder <akhad007@ucr.edu>
2025-05-29 18:21:35 -0700

    firebase

--- backend/custom_detect.py
+++ backend/custom_detect.py
@@ -5,6 +5,12 @@ import argparse
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput, Log
 
+import firebase_admin
+from firebase_admin import credentials, db
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # parse the command line
 parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
                                  formatter_class=argparse.RawTextHelpFormatter, 
@@ -14,6 +20,10 @@ parser.add_argument("input", type=str, default="", nargs='?', help="URI of the i
 parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+fb_db_path = os.getenv('FB_DB_PATH')
 
 try:
 	args = parser.parse_known_args()[0]
@@ -31,6 +41,8 @@ net = detectNet(model="/jetson-inference/python/training/detection/ssd/models/fr
                 input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
                 threshold=args.threshold)
 
+db_ref = db.reference(fb_db_path)
+
 # capture the next image
 img = input.Capture()
     
@@ -41,7 +53,11 @@ detections = net.Detect(img, overlay=args.overlay)
 print("detected {:d} objects in image".format(len(detections)))
 
 for detection in detections:
-    print("OBJECT DETECTED: " net.GetClassLabel(detection.ClassID))
+    data = {
+        'itemName': net.GetClassDesc(detection.ClassID),
+        'quantity': 1
+    }
+    db_ref.push(data)
     print(detection)
 
 # render the image
